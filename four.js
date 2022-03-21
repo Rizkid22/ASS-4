6426b0ef8ebb3a94a2b395ddfc9a289ba52583ad
lawal ridwan <lawalridwanullahi20@gmail.com>
2022-03-21 05:58:49 -0700

    Added js and html file

---
+++ four.js
@@ -0,0 +1,6 @@
+var myString;
+console.log('i think the value of the string should be ......i am a boy');
+console.log(myString);
+myString="i am a girl";
+console.log('i think i am a girl will be logged to the console');
+console.log(myString);
